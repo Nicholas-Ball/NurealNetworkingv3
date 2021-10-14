37f6281c46a0655188d268d4d956e8c3d734005c
Nicholas Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2021-10-14 18:24:16 +0000

    backup

--- test/main.cpp
+++ test/main.cpp
@@ -9,13 +9,12 @@
 
 int main() 
 {
-  std::cout << QMath::POW(-3.3,2)<<std::endl;
   Brainz::Basic b;
   
   nlohmann::json j;
 
   std::vector<double> inp = {3,2,3}; 
-  std::vector<double> out = {1,20}; 
+  std::vector<double> out = {1,2}; 
 
   std::vector<std::vector<double>> outs = {out,out};
   std::vector<std::vector<double>> inps = {inp,inp};
